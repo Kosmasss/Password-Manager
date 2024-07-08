c2d2facd6aa31e9bf329dbac85221b080c6f128b
george <georgekosmas08@gmail.com>
2024-07-08 22:22:25 +0300

    Update print(hello).py

--- print(hello).py
+++ print(hello).py
@@ -1,12 +1,29 @@
+def write_to_file(file_path, data):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(data)
+        print("Data written to file successfully.")
+    except Exception as e:
+        print(f"An error occurred while writing to the file: {e}")
 
+def read_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = file.read()
+        print("Data read from file successfully.")
+        return data
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        return None
+    
+file_path = 'example.txt'
 search=input("search")
+data_from_file = read_from_file(file_path)
 
- 
-Site = ("Microsoft","Ubuntu","Ebay")
 
-Password = [1234,4444.3333]
 
-if search==Site :
- print(Password)
+if search==data_from_file :
+ print(data_from_file)  
 else :
  print("Site does not exist")
+ Data =input("add site")
